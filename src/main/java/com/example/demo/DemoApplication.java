a7c72b85f7ca393e3f013a5f36b8cd7ce3be2df1
nour <NourElHouda.TRABELSI@esprit.tn>
2025-05-13 02:10:29 +0100

    Ajout d'un commentaire pour déclencher le pipeline Jenkins

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+
     public static void main(String[] args) {
+        // Commentaire pour déclencher le pipeline Jenkins
         SpringApplication.run(DemoApplication.class, args);
     }
 
